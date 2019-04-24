cd8d08aa6f84130304e21b86d07c335a5dfdc4ec
vc-dhavaljoshi <dhaval.joshi@viitor.cloud>
2019-04-24 18:16:39 +0530

    Coveralls file modification

---
+++ .atoum.php
@@ -0,0 +1,19 @@
+<?php
+use mageekguy\atoum;
+use mageekguy\atoum\reports;
+
+$coveralls = new reports\asynchronous\coveralls('src', '0BCAbyO7038xwKlJFyvaDk3tbXdNPTaUg');
+$defaultFinder = $coveralls->getBranchFinder();
+$coveralls->setBranchFinder(function() use ($defaultFinder) {
+        if (($branch = getenv('TRAVIS_BRANCH')) === false)
+        {
+            $branch = $defaultFinder();
+        }
+        return $branch;
+    })
+    ->setServiceName(getenv('TRAVIS') ? 'travis-ci' : null)
+    ->setServiceJobId(getenv('TRAVIS_JOB_ID') ?: null)
+    ->addWriter();
+$runner->addReport($coveralls);
+
+$script->addDefaultReport();
